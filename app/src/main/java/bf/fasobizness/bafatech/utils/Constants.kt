20c9a6d814c5d7600a7c6dfa667465daa7f994a6
Arthur <r.thur.light@gmail.com>
2020-12-14 16:00:34 +0000

    Change message style

---
+++ app/src/main/java/bf/fasobizness/bafatech/utils/Constants.kt
@@ -0,0 +1,18 @@
+package bf.fasobizness.bafatech.utils;
+
+
+class Constants {
+
+    companion object HOST {
+        fun api_server_url() : String{
+            RemoteConfigUtils.init()
+            return RemoteConfigUtils.getServerUrl()
+        }
+    }
+
+    // public static final String HOST_URL = "https://fasobizness.com/api/public/";
+    // public static final String HOST_URL = "http://192.168.0.105/api/public/";
+    // public static final String HOST_URL = "http://10.0.2.2:8080/";
+    // public static final String HOST_URL = "http://192.168.43.236:8000/";
+    // public static final String HOST_URL = "http://192.168.2.142:8080/";
+}
